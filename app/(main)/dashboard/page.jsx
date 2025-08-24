5b7c01297a3ebebfb1d4ba0091e28f8f12004633
DenilGevariya <gevariyadenil0055@gmail.com>
2025-08-24 21:10:53 +0530

    finaldone

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -6,13 +6,16 @@ import DashboardView from "./_components/dashboard-view";
 
 const IndustryInsightsPage = async () => {
   const { isOnboarded } = await getUserOnboardingStatus();
-  const insights= await getIndustryInsights();
   if (!isOnboarded) {
     redirect("/onboarding");
   }
-  return <div className="container mx-auto">
-    <DashboardView insights={insights}/>
-  </div>;
+  const insights = await getIndustryInsights();
+
+  return (
+    <div className="container mx-auto">
+      <DashboardView insights={insights} />
+    </div>
+  );
 };
 
 export default IndustryInsightsPage;
